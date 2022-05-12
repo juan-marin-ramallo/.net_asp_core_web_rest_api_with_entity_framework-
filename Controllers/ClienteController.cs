80b8fb2c8e21be5e0810ff89994ab802cea5d07e
Juan Marin <jmarin@espol.edu.ec>
2022-05-11 23:55:12 -0500

    Add files via upload

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebRestAPI.Models;
+
+namespace WebRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClienteController : ControllerBase
+    {
+        private readonly ReservaCochesContext _context;
+
+        public ClienteController(ReservaCochesContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Cliente
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes(int? codGarante, int? posicionElementoEmpezar, int? cantidadElementosTraer)
+        {
+            var clientes = _context.Clientes.AsQueryable();
+
+            if (codGarante != null)
+                clientes = _context.Clientes.Where(c => c.CodGarante == codGarante);
+
+            if (posicionElementoEmpezar != null)
+                clientes = clientes.Skip((int)posicionElementoEmpezar);
+
+            if (cantidadElementosTraer != null)
+                clientes = clientes.Take((int)cantidadElementosTraer);
+
+            return await  clientes.ToListAsync();
+        }
+
+        // GET: api/Cliente
+        //[HttpGet]
+        /*public async Task<ActionResult<IEnumerable<Cliente>>> GetClientes()
+        {
+          if (_context.Clientes == null)
+          {
+              return NotFound();
+          }
+            return await _context.Clientes.ToListAsync();
+        }*/
+
+        // GET: api/Cliente/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Cliente>> GetCliente(int id)
+        {
+          if (_context.Clientes == null)
+          {
+              return NotFound();
+          }
+            var cliente = await _context.Clientes.FindAsync(id);
+
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            return cliente;
+        }
+
+        // PUT: api/Cliente/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutCliente(int id, Cliente cliente)
+        {
+            if (id != cliente.CodCliente)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(cliente).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ClienteExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Cliente
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Cliente>> PostCliente(Cliente cliente)
+        {
+          if (_context.Clientes == null)
+          {
+              return Problem("Entity set 'ReservaCochesContext.Clientes'  is null.");
+          }
+            _context.Clientes.Add(cliente);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetCliente", new { id = cliente.CodCliente }, cliente);
+        }
+
+        // DELETE: api/Cliente/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCliente(int id)
+        {
+            if (_context.Clientes == null)
+            {
+                return NotFound();
+            }
+            var cliente = await _context.Clientes.FindAsync(id);
+            if (cliente == null)
+            {
+                return NotFound();
+            }
+
+            _context.Clientes.Remove(cliente);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ClienteExists(int id)
+        {
+            return (_context.Clientes?.Any(e => e.CodCliente == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ Models/ReservaCoche.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WebRestAPI.Models
+{
+    public partial class ReservaCoche
+    {
+        public int NumeroReserva { get; set; }
+        public string Placa { get; set; } = null!;
+        public byte GalonesGasolina { get; set; }
+
+        public virtual Reserva NumeroReservaNavigation { get; set; } = null!;
+        public virtual Coche PlacaNavigation { get; set; } = null!;
+    }
+}
